cf61a917f4a4c45ccaf25c0f03c3020bf302f5ac
bunshock <bunshock@bitbucket.org>
2020-10-22 13:47:21 -0300

    Made axios url variable for future changes

--- src/components/game/Vote.js
+++ src/components/game/Vote.js
@@ -5,6 +5,7 @@ import './Vote.css';
 const Vote = ({gameState}) => {
   let lumosRef = useRef();
   let noxRef = useRef();
+  const url = 'https://jsonplaceholder.typicode.com/posts/2';
 
   const disableButtons = () => {
     lumosRef.current.setAttribute("disabled", "disabled");
@@ -13,11 +14,8 @@ const Vote = ({gameState}) => {
   const vote = (e) => {
     let vote = e.target.className.split(' ')[0];
     console.log(vote);
-    axios.put('https://jsonplaceholder.typicode.com/posts/2', {vote: vote === 'lumos'} )
-      .then(res => {
-        console.log(res, res.status === 200 ? 'SUCCESS!' : 'ERROR')
-      });
-      disableButtons();
+    axios.put(url, {vote: vote === 'lumos'} );
+    disableButtons();
   }
   return gameState === 1 ? (
     <div className="vote">
