600ecaa88630d1711c8fa144af8c1c9a5a3295be
LuisRamirez <ramirezluis0111@gmail.com>
2020-10-26 10:15:44 -0300

    Code cleaning in App

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ const divStyle = {
 };
 
 function App() {
-    // const [searchTerm, setSearchTerm] = React.useState("");
-    // const handleChange = event => {
-    //     setSearchTerm(event.target.value);
-    //   };
   return (
     <div className="App">
         <h1 style={divStyle}>Unirse a Partida</h1>
