f94d2e3b6fb3d908640344a25f2c674e7338ee38
unknown <jere_marin@hotmail.com>
2020-10-30 17:56:54 -0300

    agregando feature EA-26

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Register from './pages/register/register'
+//instalar esta librerianpm
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+//en la ruta / estara el componente register y login.
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+                <Switch>
+                <Route exact path="/" component={Register} />
+                </Switch>
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                  
+      </Router>
     </div>
   );
 }
