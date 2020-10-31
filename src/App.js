cf9bd998adc7d501227283e083984a87becb0eae
unknown <jere_marin@hotmail.com>
2020-10-31 19:06:21 -0300

    arreglando manqueada mia

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import PageCreateGame from'./pages/creategame/PageCreateGame';
-import Register from './pages/register/register'
-import PersonList from './pages/joinagame/Lists'
-//import LobbyPage from './components/Lobby'
 
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
-//<Route path="/Lobby"><LobbyPage /></Route>
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/Home" component={PageCreateGame} ></Route>
-                <Route exact path="/" component={Register}></Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <div className="App">
+      
+    </div>
+  );
 }
 
 export default App;
