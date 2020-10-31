e300e9888d03d0182881a91cc5a2573ab7c2450e
bunshock <bunshock@bitbucket.org>
2020-10-31 14:07:53 -0300

    Disable buttons when pressed functionality

--- src/components/game/EmitProclamation.js
+++ src/components/game/EmitProclamation.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import axios from 'axios';
 import configData from '../../config.json';
 import './EmitProclamation.css';
 
 const EmitProclamation = ({phase, cards, headmaster, gameId}) => {
+  let card1 = useRef();
+  let card2 = useRef();
+
+  const disableButtons = () => {
+    card1.current.setAttribute("disabled", "disabled");
+    card2.current.setAttribute("disabled", "disabled")
+  }
 
   const choose = (e) => {
     let choice = e.target.className.split(' ')[0];
@@ -15,12 +22,14 @@ const EmitProclamation = ({phase, cards, headmaster, gameId}) => {
         }
       }
     );
+
+    disableButtons();
   }
 
   return phase === 'EMIT_PROC' /*&& headmaster === current player user_id???*/? (
     <div id="proclam">
-      <button className="phoenix" onClick={(e) => {choose(e)}}></button>
-      <button className="death" onClick={(e) => {choose(e)}}></button>
+      <button className="phoenix" ref={card1} onClick={(e) => {choose(e)}}></button>
+      <button className="death" ref={card2} onClick={(e) => {choose(e)}}></button>
     </div>
   ) : (
     <p></p>
