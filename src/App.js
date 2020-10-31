d094d4115368657f51e3faad4a82b7b40e46f333
unknown <jere_marin@hotmail.com>
2020-10-31 20:09:34 -0300

    arreglando bugs

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import PageCreateGame from'./pages/creategame/PageCreateGame';
 import Register from './pages/register/register'
 import PersonList from './pages/joinagame/Lists'
 //import LobbyPage from './components/Lobby'
+import PrincipalPage from './principalpage/principalpage';
 
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 //<Route path="/Lobby"><LobbyPage /></Route>
-function App() {
-    return (
+function App() { 
+  return (
         <Router>
             <Switch>
-                <Route exact path="/Home" component={PageCreateGame} ></Route>
+                <Route exact path="/Home" component={PrincipalPage} ></Route>
                 <Route exact path="/" component={Register}></Route>
             </Switch>
         </Router>
