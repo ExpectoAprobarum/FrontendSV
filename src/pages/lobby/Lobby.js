5dd929b9a934833d5b584753ac28b5d8326354d4
LuisRamirez <ramirezluis0111@gmail.com>
2020-11-02 15:24:54 -0300

    corregido parte del Lobby

--- src/pages/lobby/Lobby.js
+++ src/pages/lobby/Lobby.js
@@ -47,9 +47,31 @@ export default class LobbyPage extends React.Component {
         })
     };
 
+    gameStart(){
+        console.log("IDPART: ", parseInt(this.props.location.state.gameId))
+        const idPart = parseInt(this.props.location.state.gameId)
+        const usertoken = localStorage.getItem('user')
+
+        console.log("token: ", JSON.parse(usertoken).access_token)
+        axios.post(`http://127.0.0.1:8000/games/${idPart}/start`,({}),{
+            headers: {
+                'Authorization': `Bearer ${JSON.parse(usertoken).access_token}` 
+            }
+        }).then(response => { 
+            if(response.status === 200){
+                const response_id = response.data
+                console.log("idCorrecto:", response_id)
+            }
+        })
+        .catch(error => {
+           console.log(error)
+        })
+    }
+
     render() {
         if (this.state.initPartida) {
-            return <div><Game gameId={this.props.location.aboutProps.gameId}/></div>
+            this.gameStart()
+            return <div><Game gameId={parseInt(this.props.location.state.gameId)}/></div>
         }
         return (
             <div>
