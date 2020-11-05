8307e01de82b146a72f455659f746ca92bc12754
bunshock <bunshock@bitbucket.org>
2020-11-05 19:50:38 -0300

    Rewritten EmitProclamation component from class to function

--- src/pages/game/components/EmitProclamation.js
+++ src/pages/game/components/EmitProclamation.js
@@ -1,31 +1,22 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import configData from '../../../config.json';
 import './EmitProclamation.css';
 
-//props: phase, gameId, userId, headmaster (id)
-class EmitProclamation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      player: {},
-      cards: []
-    }
-  }
+const EmitProclamation = ({gameId}) => {
+  const [player, setPlayer] = useState({});
+  const [cards, setCards] = useState([]);
 
-  getPlayerInfo = () => {
+  const getPlayerInfo = () => {
     const usertoken = localStorage.getItem('user');
-    axios.get(configData.API_URL + '/games/' + this.props.gameId + '/me', {
+    axios.get(configData.API_URL + '/games/' + gameId + '/me', {
       headers: {
           'Authorization': `Bearer ${JSON.parse(usertoken).access_token}` 
         }
       })
       .then(res => {
         if(res.status === 200) {
-          let playerData = res.data
-          this.setState({
-            player: playerData
-          })
+          setPlayer(res.data);
         }
       })
       .catch(error => {
@@ -33,19 +24,16 @@ class EmitProclamation extends Component {
       })
   }
 
-  getCards = () => {
+  const getCards = () => {
     const usertoken = localStorage.getItem('user');
-    axios.get(configData.API_URL + '/games/' + this.props.gameId + '/proclamations', {
+    axios.get(configData.API_URL + '/games/' + gameId + '/proclamations', {
       headers: {
           'Authorization': `Bearer ${JSON.parse(usertoken).access_token}` 
         }
       })
       .then(res => {
         if(res.status === 200) {
-          let newCards = res.data.data;
-          this.setState({
-            cards: newCards
-          })
+          setCards(res.data.data);
         }
       })
       .catch(error => {
@@ -53,10 +41,10 @@ class EmitProclamation extends Component {
       })
   }
 
-  choose = (e) => {
+  const choose = (e) => {
     let choice = e.target.className.split(' ')[0];
     const usertoken = localStorage.getItem('user');
-    axios.post(configData.API_URL + '/games/' + this.props.gameId + '/proclamations',
+    axios.post(configData.API_URL + '/games/' + gameId + '/proclamations',
       { card: choice }, {
         headers: {
             'Authorization': `Bearer ${JSON.parse(usertoken).access_token}` 
@@ -75,21 +63,19 @@ class EmitProclamation extends Component {
     document.getElementById("proc2").disabled = true;
   }
 
-  componentDidMount() {
-    this.getPlayerInfo();
-    this.getCards();
-  }
+  useEffect(() => {
+    getPlayerInfo();
+    getCards();
+  })
 
-  render() {
-    return this.state.player.current_position === "headmaster" ? (
-      <div className="proclam">
-        <button className={this.state.cards[0] + " card left"} id="proc1" onClick={this.choose}></button>
-        <button className={this.state.cards[1] + " card right"} id="proc2" onClick={this.choose}></button>
-      </div>
-    ) : (
-      <p></p> 
-    )
-  }
+  return player.current_position === "headmaster" ? (
+    <div className="proclam">
+      <button className={cards[0] + " card left"} id="proc1" onClick={(e) => {choose(e)}}></button>
+      <button className={cards[1] + " card right"} id="proc2" onClick={(e) => {choose(e)}}></button>
+    </div>
+  ) : (
+    <p></p> 
+  )
 }
 
 export default EmitProclamation
