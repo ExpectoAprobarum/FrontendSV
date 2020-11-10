2b2244c6666c6820694d9f6edf0c75fdaff2c766
Leandro Mantovani <57453958+Bunshock@users.noreply.github.com>
2020-11-10 18:07:16 -0300

    changed invitate to invite

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import LobbyPage from './pages/lobby/Lobby'
 import Register from './pages/register/register'
 import Login from './pages/login/login'
-import Invitate from './pages/invitate/invitateURL'
+import Invite from './pages/invite/inviteURL'
 import {
     BrowserRouter as Router,
     Switch,
@@ -20,7 +20,7 @@ function App() {
                 <Route exact path="/register" component={Register}></Route>
                 <Route exact path="/login" component={Login}></Route>
                 <Route exact path="/" component={RegisterAndLogin}></Route>
-                <Route path="/Game/invitate" component={Invitate}></Route>
+                <Route path="/Game/invite" component={Invite}></Route>
             </Switch>
         </Router>
     );
