b4c0ec909ee6ac510ef0611cab9a14c01b76391e
bunshock <bunshock@bitbucket.org>
2020-11-29 15:11:42 -0300

    Refactor of player selection phases and modified style files to match new changes

--- src/pages/game/components/spells/AvadaKedavra.js
+++ src/pages/game/components/spells/AvadaKedavra.js
@@ -1,61 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import configData from '../../../../config.json';
-import { getPlayers } from '../../../../commons/players/players';
-import PlayerList from '../PlayerList';
-import { notify_player_choose_err } from '../../../../commons/alerts/toast';
+import React from 'react';
+import SelectPlayer from '../SelectPlayer';
 import './SpellObjective.css';
 
 const AvadaKedavra = ({gameId, ministerId}) => {
-  const [selected, setSelection] = useState(0);
-  const [players, setPlayers] = useState([]);
-
-  useEffect(() => {
-    getPlayers(gameId)
-      .then(res => {
-        setPlayers(res)
-      });
-  }, [gameId]);
-
-  const selectPlayer = (id) => {
-    setSelection(id);
-  }
-
-  const sendElection = () => {
-    const usertoken = localStorage.getItem('user');
-    axios.post(configData.API_URL + '/games/' + gameId + '/avadakedavra',
-      { "id": selected }, {
-      headers: {
-          'Authorization': `Bearer ${JSON.parse(usertoken).access_token}`
-        }
-      })
-      .then(res => {
-        console.log(res.status)
-      })
-      .catch(error => {
-        notify_player_choose_err();
-      })
-  }
-
   return (
     <div className="Spell">
-      <h2 className="spell-header">Select player to cast: Avada Kedavra</h2>
-      <div className="player-list">
-        <PlayerList
-          selectPlayer={selectPlayer}
-          selected={selected}
-          players={players}
-          showCond={["alive", true]}
-          chooseCond={["alive", true]}
-          minister={[ministerId, false]}
-        />
-      </div>
-      <button className="sendSpellElection" id="sendSpellElection"
-        onClick={() => {
-          sendElection()
-        }}>
-        Choose
-      </button>
+      <SelectPlayer 
+        gameId={gameId}
+        ministerId={ministerId}
+        phase={'avadakedavra'}
+      />
     </div>
   )
 }
